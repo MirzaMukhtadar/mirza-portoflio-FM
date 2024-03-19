415c264d4ca4e079a89e1e89c2ea3a2e92e8f027
john Doe <john@gmail.com>
2024-03-19 23:28:43 +0530

    added services section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar";
 
 import Hero from "./components/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
 
 const App = () => {
   return (
@@ -15,7 +16,9 @@ const App = () => {
       <section id="Services">
         <Parallax type="services" />
       </section>
-      <section>Services</section>
+      <section>
+        <Services />
+      </section>
       <section id="Portfolio">
         <Parallax type="portfolio" />
       </section>
